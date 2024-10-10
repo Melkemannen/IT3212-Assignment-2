ddd031dffa329e21ea40604b64e751e73e85c2f6
Aksel Lessum <alessum@hotmail.no>
2024-10-10 15:04:48 +0200

    Almost finished PCA

--- PCA/pca.py
+++ PCA/pca.py
@@ -3,6 +3,7 @@ import cv2
 import os
 import numpy as np
 from numpy import linalg as LA
+import matplotlib.pyplot as plt
 
 folder_path = 'C:/Users/aless/Documents/NTNU/Datadrevet/IT3212-Assignment-2/PCA/hatchback'
 
@@ -18,7 +19,7 @@ for filename in os.listdir(folder_path):
 
 
 # Normalize pixel values
-scaled_images = []
+scaled_images = []  
 
 for filename in os.listdir(folder_path):
     img_path = os.path.join(folder_path, filename)
@@ -38,11 +39,65 @@ for imageArray in scaled_images:
 images_2d = np.array(images_2d)
 
 # Calculate the covariance matrix
+imageMean = np.mean(images_2d, axis=0)
+images_2d = images_2d - np.mean(images_2d, axis=0)
 cov_matrix = np.cov(images_2d, rowvar=False)
 
 # Calculate the eigenvectors and eigenvalues of the covariance matrix
 start_time = time.time()
 eigenvalues, eigenvectors = LA.eig(cov_matrix)
-print(eigenvalues)
-print(eigenvectors)
-print(time.time()-start_time)
+eigenvalues = np.real(eigenvalues)
+eigenvectors = np.real(eigenvectors)
+
+# Sort eigenvectors based on eigenvalues in descending order
+vectorDict = {}
+for i in range(0, len(eigenvalues)):
+    vectorDict[tuple(eigenvectors[:,i])] = eigenvalues[i]
+
+vectorDict = dict(sorted(vectorDict.items(), key=lambda item: item[1], reverse=True))
+
+# Get the top k eigenvectors to create principal components
+k = 40
+PCs =  list(vectorDict.keys())[:k]
+PCs = np.array(PCs)
+
+# Project images onto lower dimensionality space (k dimensions)
+reducedImages = np.dot(images_2d, PCs.T)
+
+# Reconstruct images using principal components
+reconstructedImages = np.dot(reducedImages, PCs) + imageMean
+
+reconstructedImages = reconstructedImages.reshape(-1, 50, 50)
+
+'''if np.iscomplexobj(reconstructedImages):
+    reconstructedImages = np.real(reconstructedImages)'''
+
+plt.figure(figsize=(12, 6))
+for i in range (0,6):
+    plt.subplot(2,3,i+1)
+    plt.imshow(reconstructedImages[i], cmap='gray')
+    plt.axis('off')
+
+plt.tight_layout()
+plt.show()
+
+# Calculate and plot the amount of variance contained in the principal components
+variances_k = []
+totalVariance = np.sum(eigenvalues)
+
+for i in range (0, 200):
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    sorted_eigenvalues = eigenvalues[sorted_indices]
+    contained_variance = np.sum(sorted_eigenvalues[:i]) / totalVariance
+    variances_k.append(contained_variance)
+    
+plt.figure(figsize=(12, 6))
+plt.plot(variances_k, label='Variance retained by k PCs')
+plt.axhline(y=1, color='r', linestyle='--', linewidth=2, label='Total Variance')
+plt.xticks(np.arange(0, len(variances_k), 10))
+plt.xlabel('k-PCs')
+plt.ylabel('Variance')
+
+plt.legend()
+plt.grid()
+plt.show()
